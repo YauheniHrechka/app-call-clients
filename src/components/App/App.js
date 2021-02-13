b5f4c74c2621e16e53a56d7c553739a4142ef59d
Yauheni Hrechka <ip.grechko.ev@gmail.com>
2021-02-13 16:04:07 +0300

    Added a project template. Added the following components: 'App', 'Header' and 'Items'.

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.scss';
+
+import Header from '../Header/Header';
+
+const App = () => {
+
+  React.useEffect(() => {
+
+
+
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
